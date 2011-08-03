1f972d00f84a5ce579afd62ea0dc52d572218f7f
arzynik <github@arzynik.com>
2011-08-03 23:34:53 +0000

    initial oo commit

--- cpanel-import.php
+++ cpanel-import.php
@@ -6,7 +6,7 @@
  *
  * The purpose of this script is to import cpanel backups into a system that uses no cpanel
  *
- * usage: cpanel-import.php username password [domainname] [groupname] [ip]
+ * usage: cpanel-import.php file [options]
  *
  * this script currently only supports:
  *   creation of users and assigning to a group
@@ -15,209 +15,363 @@
  *   creationg of log directories
  *   creation of mysql dbs and users
  */
- 
+
+
+// look for some errors
 error_reporting(E_ALL ^ E_NOTICE);
 ini_set('display_errors',true);
 set_time_limit(0);
-$config = new stdClass;
+date_default_timezone_set('America/Los_Angeles');
 
 
-/*
- * edit this config to properly use this script
- */
+// set up the config
+$config = new CpanelImport_Config;
+$config->loadArgs($argv);
 
-// the source of where the backups are stored. backups need to be named like: USER.tar.gz
-$config->source = '/home/devin/backups/';
-// the path to your apache files. usually httpd or apache2
-$config->http = '/etc/httpd/';
-// where you want your users created
-$config->dest = '/home/';
-// your mysql username that has grant and create db permissions. probably root
-$config->mysqlUser = 'root';
-// your mysql password
-$config->mysqlPass = null;
+// import!
+$import = new CpanelImport($config);
+$import->import();
 
 
-/*
- * you probably wont need to edit anything below this line
- */
- 
-// this template is created for apache in the sites-available path. edit this template if you want some suexec or something
-$config->hostTemplate = '
-<VirtualHost '.($config->ip ? $config->ip.':80' : '*').'>
-	ServerName _DOMAIN_
-	ServerAlias *._DOMAIN_ _DOMAINS_
-	DocumentRoot /home/_USER_/www
-	ServerAdmin webmaster@_DOMAIN_
-	UseCanonicalName Off
-	ErrorLog /home/_USER_/logs/error_log
-	CustomLog /home/_USER_/logs/access_log combined
-</VirtualHost>
-';
-// optional: if you want a different format of file name
-$config->file = $config->source.$config->user.'.tar.gz';
-// the groupname. you can pass this in arguments
-$config->groupname = $argv[4] ? $argv[4] : 'web';
-// the ip to bind to. you can pass this in arguments
-$config->ip = $argv[5] ? $argv[5] : null;
-// the files to NOT copy over form the homedir
-$config->ignoreFiles = array(
-	'mail',
-	'public_ftp',
-	'cpbackup.*',
-	'backup.*',
-	'access-logs',
-	'tmp',
-	'logs',
-	'www',
-	'.lastlogin',
-	'.htpasswds',
-	'.cpanel',
-	'etc',
-	'.ftpquota',
-	'.autorespond',
-	'.filter',
-	'.spamkey',
-	'.lang',
-	'cpmove.psql',
-	'.cpaddons',
-	'account-locked.tpl',
-	'.bash_history'
-);
-// you can add a prefix or something to the user if you would like
-$config->user = $argv[1];
-// the command to auth. probably wont need to edit this
-$config->mysqlAuth = ' -u '.$config->mysqlUser.($config->mysqlPass ? ' -p '.$config->mysqlPass .' ' : '');
-
-
-if (!$config->user) {
-	die('usage: cpanel-import.php username password [domainname] [groupname] [ip]');
-}
-
-if (!file_exists($config->file)) {
-	die('file "'.$config->file.'" does not exist.');
-}
-
-
-// create the user and give it a password
-exec('groupadd '.$config->groupname);
-exec('useradd '.$config->user.' -g '.$config->groupname);
-exec('echo '.$argv[2].' | passwd '.$config->user.' --stdin');
-
-
-// create the directory for working with
-exec('rm -Rf '.$config->source.$config->user);
-exec('mkdir '.$config->source.$config->user);
-
-
-// create the user directory
-exec('rm -Rf '.$config->dest.$config->user);
-exec('mkdir '.$config->dest.$config->user);
-exec('mkdir '.$config->dest.$config->user.'/logs');
-
-
-// extract the files
-exec('tar xzf '.$config->file.' -C '.$config->source.$config->user);
-exec('tar xf '.$config->source.$config->user.'/*/homedir.tar -C '.$config->source.$config->user.'/*/homedir');
-
-
-// set the extracted path
-$config->extracted = exec('cd '.$config->source.$config->user.'/*/ && pwd').'/';
-
-
-// move all web files
-$iterator = new DirectoryIterator($config->extracted.'homedir');
-foreach ($iterator as $fileinfo) {
-	if ($fileinfo->isDot()) {
-		continue;
+class CpanelImport {
+	public function __construct(CpanelImport_Config $config) {
+		$this->config = $config;
 	}
-	$copy = true;
-	foreach ($config->ignoreFiles as $ignore) {
-		if (preg_match('/'.$ignore.'/',trim($fileinfo->getFilename()))) {
-			$copy = false;
+	
+	public function import() {
+		$this->files = new CpanelImport_Files($this);
+		$this->files->extract();
+		
+		$this->group = new CpanelImport_Group($this);
+		$this->group->create();
+		
+		$this->user = new CpanelImport_User($this);
+		$this->user->create();
+		
+		$this->user = new CpanelImport_Vhost($this);
+		$this->user->write();
+		
+		$this->user = new CpanelImport_Mysql($this);
+		$this->user->create();
+		
+		$this->files->move();
+		//$this->files->remove();
+
+		// restart apache
+		CpanelImport::exec('/etc/init.d/httpd restart');
+	}
+	
+	public static function exec($cmd) {
+		//if ($this->config->debug) {
+			echo "Running command:\n".$cmd."\n\n";
+		//∂}
+		return exec($cmd);
+	}
+	
+	public function usage() {
+		echo 'fuck you';
+	}
+}
+
+
+class CpanelImport_Config {
+	
+	// the source of where the backups are stored. backups need to be named like: USER.tar.gz
+	public $source = '/home/devin/backup/';
+
+	// the path to your apache files. usually httpd or apache2
+	public $http = '/etc/httpd/';
+
+	// where you want your users created
+	public $dest = '/home/';
+
+	// your mysql username that has grant and create db permissions. probably root
+	public $mysqlUser = 'root';
+
+	// your mysql password
+	public $mysqlPass = null;
+
+	// this template is created for apache in the sites-available path. edit this template if you want some suexec or something
+	public $hostTemplate = '
+	<VirtualHost _IP_>
+		ServerName _DOMAIN_
+		ServerAlias *._DOMAIN_ _DOMAINS_
+		DocumentRoot /home/_USER_/www
+		ServerAdmin webmaster@_DOMAIN_
+		UseCanonicalName Off
+		ErrorLog /home/_USER_/logs/error_log
+		CustomLog /home/_USER_/logs/access_log combined
+	</VirtualHost>
+	';
+
+	// the files to NOT copy over form the homedir
+	public $ignoreFiles = array(
+		'mail',
+		'public_ftp',
+		'cpbackup.*',
+		'backup.*',
+		'access-logs',
+		'tmp',
+		'logs',
+		'www',
+		'.lastlogin',
+		'.htpasswds',
+		'.cpanel',
+		'etc',
+		'.ftpquota',
+		'.autorespond',
+		'.filter',
+		'.spamkey',
+		'.lang',
+		'cpmove.psql',
+		'.cpaddons',
+		'account-locked.tpl',
+		'.bash_history'
+	);
+	
+	//default values;
+	public $password;
+	public $username;
+	public $ip;
+	public $filename;
+	public $debug = false;
+	
+	public function loadArgs($args) {
+		$args = $this->parseArgs($args);
+		if (!$args) {
+			throw new CpanelImport_Exception;
+		}
+		
+		$this->filename = array_shift($args);
+		foreach ($args as $key => $value) {
+			if (property_exists($this,$key)) {
+				switch ($key) {
+					case 'p':
+					case 'pasword':
+						$this->password = $value;
+						break;
+					case 'debug':
+						$this->debug = true;
+						break;
+				}
+			}
+		}
+	
+	}
+	
+	private function parseArgs($argv){
+		array_shift($argv); $o = array();
+		foreach ($argv as $a){
+			if (substr($a,0,2) == '--'){ $eq = strpos($a,'=');
+				if ($eq !== false){ $o[substr($a,2,$eq-2)] = substr($a,$eq+1); }
+				else { $k = substr($a,2); if (!isset($o[$k])){ $o[$k] = true; } } }
+			else if (substr($a,0,1) == '-'){
+				if (substr($a,2,1) == '='){ $o[substr($a,1,1)] = substr($a,3); }
+				else { foreach (str_split(substr($a,1)) as $k){ if (!isset($o[$k])){ $o[$k] = true; } } } }
+			else { $o[] = $a; } }
+		return $o;
+	}
+}
+
+
+class CpanelImport_Group {
+	public $groupname;
+
+	public function __construct($import) {
+		$this->groupname = $this->import->config->groupname ? $this->import->config->groupname : 'web';
+	}
+
+	public function create() {
+		CpanelImport::exec('groupadd '.$this->import->config->groupname);
+	}
+}
+
+
+class CpanelImport_User {
+	public $username;
+	public $password;
+	public $shadow;
+	public $cpanelUsername;
+
+	public function __construct($import) {
+		$this->import = $import;
+		$this->cpanelUsername = trim(CpanelImport::exec('ls '.$this->import->files->extracted.'cp'));
+		$this->password = $this->import->config->password;
+		$this->shadow = trim(file_get_contents($this->import->files->extracted.'shadow'));
+		$this->username = $this->import->config->username ? $this->import->config->username : $this->cpanelUsername;
+	}
+	
+	public function create() {
+		CpanelImport::exec('useradd '.$this->username.' -g'.$this->import->group->groupname);
+		if ($this->password) {
+			CpanelImport::exec('echo '.$argv[2].' | passwd '.$this->username.' --stdin');
+		} else {
+			CpanelImport::exec('awk -F: \'/'.$this->username.'/ { OFS=":"; $2="'.$this->shadow.'"; print}\' /etc/shadow');
 		}
 	}
-	if ($copy) {
-		echo 'Copying: '.$config->extracted.'homedir/'.$fileinfo->getFilename()."\n";
-		exec('mv '.$config->extracted.'homedir/'.$fileinfo->getFilename().' '.$config->dest.$config->user.'/');
+}
+
+
+class CpanelImport_Files {
+	public $extracted;
+	private $basename;
+
+	public function __construct($import) {
+		$this->import = $import;
+		if (!file_exists($this->import->config->source.$this->import->config->filename)) {
+			throw new CpanelImport_Exception('The file "'.$this->import->config->filename.'" does not exist!');
+		}
+		$this->basename = 'restore-'.substr($this->import->config->filename,0,strpos($this->import->config->filename,'.')).'-'.date('YmdHis').'.tmp';
+	}
+
+	public function extract() {
+		// extract the files
+		mkdir($this->import->config->source.$this->basename);
+		CpanelImport::exec('tar xzf '.$this->import->config->source.$this->import->config->filename.' -C '.$this->import->config->source.$this->basename);
+		CpanelImport::exec('tar xf '.$this->import->config->source.$this->basename.'/*/homedir.tar -C '.$this->import->config->source.$this->basename.'/*/homedir');
+		$this->extracted = exec('cd '.$this->import->config->source.$this->basename.'/*/ && pwd').'/';
+	}
+	
+	public function move() {
+		// create the user directory
+		CpanelImport::exec('rm -Rf '.$this->import->config->dest.$this->import->config->user);
+		CpanelImport::exec('mkdir '.$this->import->config->dest.$this->import->config->user);
+		CpanelImport::exec('mkdir '.$this->import->config->dest.$this->import->config->user.'/logs');
+		
+		// move all web files
+		$iterator = new DirectoryIterator($this->extracted.'homedir');
+		foreach ($iterator as $fileinfo) {
+			if ($fileinfo->isDot()) {
+				continue;
+			}
+			$copy = true;
+			foreach ($this->import->config->ignoreFiles as $ignore) {
+				if (preg_match('/'.$ignore.'/',trim($fileinfo->getFilename()))) {
+					$copy = false;
+				}
+			}
+			if ($copy) {
+				echo 'Copying: '.$this->extracted.'homedir/'.$fileinfo->getFilename()."\n";
+				CpanelImport::exec('mv '.$this->extracted.'homedir/'.$fileinfo->getFilename().' '.$this->import->config->dest.$this->import->user->username.'/');
+			}
+		}
+
+		// rename public_html to www because its shorter and i hate public_html
+		CpanelImport::exec('mv '.$this->import->config->dest.$this->import->user->username.'/public_html/ '.$this->import->config->dest.$this->import->config->user.'/www/');
+
+		// change permissions
+		CpanelImport::exec('chown -R '.$this->import->user->username.':'.$this->import->group->groupname.' '.$this->import->config->dest.$this->import->user->username);
+
+	}
+
+	public function remove() {
+		// remove extracted files
+		CpanelImport::exec('rm -Rf '.$this->import->config->source.$this->basename);
 	}
 }
 
 
-// rename public_html to www because its shorter and i hate public_html
-exec('mv '.$config->dest.$config->user.'/public_html/ '.$config->dest.$config->user.'/www/');
+class CpanelImport_Vhost {
+	public $domain;
+	public $domains;
 
-
-// set up the host template
-$domains = file($config->extracted.'cp/'.$config->user);
-foreach ($domains as $line) {
-	if (preg_match('/^DNS([0-9]+)?=.*$/',$line)) {
-		$doms[] = trim(preg_replace('/^DNS([0-9]+)?=(.*)$/','\\2',$line));
+	public function __construct($import) {
+		$this->import = $import;
 	}
-}
-$config->domain = $argv[3] ? $argv[3] : array_shift($doms);
-$domains = implode($doms,' ');
-
-$find = array(
-	'/_USER_/',
-	'/_DOMAIN_/',
-	'/_DOMAINS_/'
-);
-$replace = array(
-	$config->user,
-	$config->domain,
-	$domains
-);
-$template = preg_replace($find, $replace, $config->hostTemplate);
-file_put_contents($config->http.'sites-available/'.$config->user.'.conf', $template);
-exec('cd '.$config->http.'sites-enabled && ln -s ../sites-available/'.$config->user.'.conf');
-
-
-// change permissions
-exec('chown -R '.$config->user.':'.$config->groupname.' '.$config->dest.$config->user);
-
-
-// create mysql databases and grant permissions
-$iterator = new DirectoryIterator($config->extracted.'mysql/');
-foreach ($iterator as $fileinfo) {
-	if ($fileinfo->isFile() && $fileinfo->getFilename() != 'horde.sql') {
-		$db = new stdClass;
-		$db->name = $fileinfo->getBasename('.sql');
-		$db->file = $fileinfo->getFilename();
-		$databases[] = $db;
+	
+	public function read() {
+		$domains = file($this->import->files->extracted.'cp/'.$this->import->config->user);
+		foreach ($domains as $line) {
+			if (preg_match('/^DNS([0-9]+)?=.*$/',$line)) {
+				$doms[] = trim(preg_replace('/^DNS([0-9]+)?=(.*)$/','\\2',$line));
+			}
+		}
+		$this->domain = $this->import->config->domain ? $this->import->config->domain : array_shift($doms);
+		$this->domains = implode($doms,' ');
+	}
+	
+	public function write() {
+		$find = array(
+			'/_IP_/',
+			'/_USER_/',
+			'/_DOMAIN_/',
+			'/_DOMAINS_/'
+		);
+		$replace = array(
+			($this->import->config->ip ? $this->import->config->ip.':80' : '*'),
+			$this->import->user->username,
+			$this->domain,
+			$this->domains
+		);
+		$template = preg_replace($find, $replace, $this->import->config->hostTemplate);
+		file_put_contents($this->import->config->http.'sites-available/'.$this->import->user->username.'.conf', $template);
+		CpanelImport::exec('cd '.$this->import->config->http.'sites-enabled && ln -s ../sites-available/'.$this->import->user->username.'.conf');
 	}
 }
 
-function sql_write($sql, $dbname = '') {
-	global $config;
-	$tmpfile = tempnam('/tmp', 'temp-sql-import-');
-	file_put_contents($tmpfile,$sql);
 
-	if ($dbname) {
-		exec('mysql'.$config->mysqlAuth.' "'.$database->name.'" < "'.$tmpfile.'"');
-	} else {
-		exec('mysql'.$config->mysqlAuth.' < "'.$tmpfile.'"');
+class CpanelImport_Mysql {
+	private $auth;
+
+	public function __construct($import) {
+		$this->import = $import;
+		$this->auth = ' -u '.$this->import->config->mysqlUser.($this->import->config->mysqlPass ? ' -p'.$this->import->config->mysqlPass .' ' : '');
 	}
-	unlink($tmpfile);
+	
+	public function create() {
+		
+		// create mysql databases and grant permissions
+		$iterator = new DirectoryIterator($this->import->files->extracted.'mysql/');
+		foreach ($iterator as $fileinfo) {
+			if ($fileinfo->isFile() && $fileinfo->getFilename() != 'horde.sql') {
+				$db = new stdClass;
+				$db->name = $fileinfo->getBasename('.sql');
+				$db->file = $fileinfo->getFilename();
+				$databases[] = $db;
+			}
+		}
+		
+		//$this->sqlWrite("CREATE USER '".$this->import->config->user."'@'localhost' IDENTIFIED BY '".$argv[2]."';\n");
+		
+		if ($databases) {
+			$sql = '';
+			foreach ($databases as $database) {
+				$sql .= "CREATE DATABASE `".$database->name."`;\n";
+				//$sql .= "GRANT ALL ON `".$database->name."`.* TO '".$this->import->config->user."'@'localhost';\n";
+			}
+			$this->sqlWrite($sql);
+			$sql = file_get_contents($this->import->files->extracted.'mysql.sql');
+			if ($this->import->user->cpanelUsername != $this->import->user->username) {
+				$sql = str_replace("'".$this->import->user->cpanelUsername.'_',"'".$this->import->user->username.'_');
+				$sql = str_replace('`'.$this->import->user->cpanelUsername.'\_','`'.$this->import->user->username.'\_');
+			}
+			$this->sqlWrite($sql);
+			foreach ($databases as $database) {
+				CpanelImport::exec('mysql'.$this->import->config->mysqlAuth.' "'.$database->name.'" < "'.$this->import->files->extracted.'mysql/'.$database->file.'"');
+			}
+		} else {
+			echo "no databases.\n";
+		}
+	}
+
+	private function sqlWrite($sql, $dbname = '') {
+		$tmpfile = tempnam('/tmp', 'temp-sql-import-');
+		file_put_contents($tmpfile,$sql);
+		if ($dbname) {
+			CpanelImport::exec('mysql'.$this->auth.' "'.$database->name.'" < "'.$tmpfile.'"');
+		} else {
+			CpanelImport::exec('mysql'.$this->auth.' < "'.$tmpfile.'"');
+		}
+		unlink($tmpfile);
+	}
+
 }
 
-sql_write("CREATE USER '".$config->user."'@'localhost' IDENTIFIED BY '".$argv[2]."';\n");
 
-if ($databases) {
-	foreach ($databases as $database) {
-		$sql = "CREATE DATABASE `".$database->name."`;\n";
-		$sql .= "GRANT ALL ON `".$database->name."`.* TO '".$config->user."'@'localhost';\n";
-		sql_write($sql,$database->name);
-		exec('mysql'.$config->mysqlAuth.' "'.$database->name.'" < "'.$config->extracted.'mysql/'.$database->file.'"');
+class CpanelImport_Exception extends Exception {
+	public function __construct($e = null) {
+		if ($e) echo $e."\n";
+		CpanelImport::usage();
+		exit;
 	}
-} else {
-	echo "no databases.\n";
 }
-
-
-// remove extracted files
-exec('rm -Rf '.$config->source.$config->user);
-
-
-// restart apache
-exec('/etc/init.d/httpd restart');
